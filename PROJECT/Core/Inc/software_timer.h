04dde2ac704ae0e671fa205948ba09696b6ce5f3
thaole04 <thao.le14758@hcmut.edu.vn>
2023-12-11 11:49:20 +0700

    finish project

---
+++ PROJECT/Core/Inc/software_timer.h
@@ -0,0 +1,23 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Dec 11, 2023
+ *      Author: thao2
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+// timer for 3s waiting in uart communication
+extern int timer_counter;
+extern int timer_flag;
+
+// timer for blinking led
+extern int timer_blinking;
+extern int timer_blinking_flag;
+
+void setTimer(int duraion);
+void setTimerBlinking();
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
---
+++ PROJECT/Debug/Core/Src/controlData.d
@@ -0,0 +1,4 @@
+Core/Src/controlData.o: ../Core/Src/controlData.c \
+ ../Core/Inc/controlData.h
+
+../Core/Inc/controlData.h:
